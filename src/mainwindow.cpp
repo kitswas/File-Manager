57467b46abfc290246fe6f1019f99d58680c42e8
kitswas <90329875+kitswas@users.noreply.github.com>
2022-12-30 18:45:12 +0530

    Removed findChild() usages
    They were totally unnecessary.
    The UI components are stored in the pointer named ui.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -26,8 +26,7 @@ int MainWindow::add_page_to_tabpanel(QString dir, const QString &label)
 {
 	Navpage *newpage = new Navpage();
 	newpage->change_dir(dir);
-	QTabWidget *tabWidget = findChild<QTabWidget *>("tabWidget");
-	tabWidget->addTab(newpage, label);
+	ui->tabWidget->addTab(newpage, label);
 	return 0;
 }
 
@@ -38,29 +37,24 @@ void MainWindow::on_actionNew_tab_triggered()
 
 void MainWindow::on_tabWidget_currentChanged(int index)
 {
-	QTabWidget *tabWidget = findChild<QTabWidget *>("tabWidget");
-	QLineEdit *addressBar = findChild<QLineEdit *>("addressBar");
-	Navpage *currentpage = (Navpage *)(tabWidget->currentWidget());
+	Navpage *currentpage = (Navpage *) (ui->tabWidget->currentWidget());
 	if (currentpage != nullptr)
-		addressBar->setText(currentpage->get_current_dir());
+		ui->addressBar->setText(currentpage->get_current_dir());
 }
 
 void MainWindow::on_tabWidget_tabCloseRequested(int index)
 {
-	QTabWidget *tabWidget = findChild<QTabWidget *>("tabWidget");
-	tabWidget->removeTab(index);
+	ui->tabWidget->removeTab(index);
 }
 
 void MainWindow::on_addressBar_returnPressed()
 {
-	QLineEdit *addressBar = findChild<QLineEdit *>("addressBar");
-	QDir *path = new QDir(addressBar->text());
+	QDir *path = new QDir(ui->addressBar->text());
 	if (path->exists()) {
-		QTabWidget *tabWidget = findChild<QTabWidget *>("tabWidget");
-		Navpage *currentpage = (Navpage *) (tabWidget->currentWidget());
+		Navpage *currentpage = (Navpage *) (ui->tabWidget->currentWidget());
 		if (currentpage != nullptr) {
 			currentpage->change_dir(path->absolutePath());
-			tabWidget->setTabText(tabWidget->currentIndex(), path->dirName());
+			ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), path->dirName());
 		}
 	} else {
 		QMessageBox *alert = new QMessageBox();
