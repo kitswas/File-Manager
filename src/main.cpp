e8b5f2063da15a9eee939e160226250a9b72e207
kitswas <90329875+kitswas@users.noreply.github.com>
2023-01-03 20:27:36 +0530

    Set current path to home on launch

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QDir::setCurrent(QDir::homePath());
 	MainWindow w;
 	w.add_page_to_tabpanel(QDir::homePath(), "Home");
 	qDebug() << "Home" << QDir::homePath();
