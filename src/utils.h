0b85a72f96ce447d42ce8bbc87fbe76bb7637f61
kitswas <90329875+kitswas@users.noreply.github.com>
2023-01-03 20:15:01 +0530

    Added documentation

--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,15 @@
 
 #include <QtCore>
 
-QString to_higher_unit_from_bytes(qint64 bytes)
+/*!
+ * \brief Format the provided number as bytes.
+ * \param bytes
+ * \return a string
+ * 
+ * Aims to present the number in a human-friendly format.  
+ * Converts bytes to a higher order unit ("KB", "MB", "GB", "TB"), if possible. 
+ */
+QString format_bytes(qint64 bytes)
 {
 	QString result;
 	qint64 higher = bytes;
