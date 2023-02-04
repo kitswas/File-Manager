83219e8b49bad72162c9ce9bb4646161f2bc8275
kitswas <90329875+kitswas@users.noreply.github.com>
2023-02-04 22:33:20 +0530

    Split utils into header and source

--- src/utils.h
+++ src/utils.h
@@ -11,18 +11,6 @@
  * Aims to present the number in a human-friendly format.  
  * Converts bytes to a higher order unit ("KB", "MB", "GB", "TB"), if possible. 
  */
-QString format_bytes(qint64 bytes)
-{
-	QString result;
-	qint64 higher = bytes;
-	int index = 0;
-	QString units[] = {"B", "KB", "MB", "GB", "TB"};
-	while (higher > 1000) {
-		++index;
-		higher /= 1000;
-	}
-	result = QString::number(higher) + " " + units[index];
-	return result;
-}
+QString format_bytes(qint64 bytes);
 
 #endif // UTILS_H
