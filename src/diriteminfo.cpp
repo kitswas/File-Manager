680e3d64bc3a142e02f0de007be91c6c8eacdcd9
kitswas <90329875+kitswas@users.noreply.github.com>
2023-02-06 16:40:32 +0530

    NTFS permission check patch

--- src/diriteminfo.cpp
+++ src/diriteminfo.cpp
@@ -2,8 +2,12 @@
 #include "ui_diriteminfo.h"
 #include "utils.h"
 
+#ifdef Q_OS_WIN
 // See https://doc.qt.io/qt-6/qfileinfo.html#details
 extern Q_CORE_EXPORT int qt_ntfs_permission_lookup;
+#else
+int qt_ntfs_permission_lookup = 0; //dummy
+#endif
 
 DirItemInfo::DirItemInfo(QFileInfo *info, QWidget *parent)
 	: QWidget(parent)
@@ -26,7 +30,7 @@ void DirItemInfo::refresh()
 	//	qDebug() << (new QStorageInfo(QDir::current()))->fileSystemType();
 	bool isNTFS = (new QStorageInfo(QDir::current()))->fileSystemType().compare("NTFS") == 0;
 	if (isNTFS)
-		qt_ntfs_permission_lookup++; // turn checking on
+		qt_ntfs_permission_lookup++; // turn checking on, Windows only
 	if (info->isReadable())
 		permissions += "Read ";
 	if (info->isWritable())
@@ -34,7 +38,7 @@ void DirItemInfo::refresh()
 	if (info->isExecutable())
 		permissions += "Execute ";
 	if (isNTFS)
-		qt_ntfs_permission_lookup--; // turn it off again
+		qt_ntfs_permission_lookup--; // turn it off again, Windows only
 	ui->permissions->setText(permissions.trimmed());
 	ui->size->setText(format_bytes(info->size()));
 	//	info->permissions();
