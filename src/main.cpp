dbe69ed6b82355f506df8f2dbc911025c1fdd456
kitswas <90329875+kitswas@users.noreply.github.com>
2023-05-21 21:40:17 +0530

    Use Fusion Style for Windows 10+
    For Dark Mode support.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 
 #include <QApplication>
 #include <QDir>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QDir::setCurrent(QDir::homePath());
+#if defined(Q_OS_WIN)
+	if (QSysInfo::productVersion().toInt() >= 10)
+		QApplication::setStyle(QStyleFactory::create("Fusion"));
+#endif
 	MainWindow w;
 	w.add_page_to_tabpanel(QDir::homePath(), "Home");
 	qDebug() << "Home" << QDir::homePath();
